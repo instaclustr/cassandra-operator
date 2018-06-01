385822c420c9e91850086509c5ff1ee7114e6992
Adam Zegelin <adam@zegelin.com>
2018-06-01 14:38:49 -0700

    Potentially fix OkHttp leak warning. (#47)
    * Adds support for cached JXM connections to Cassandra nodes. Adds decommission support for Cassandra pods.
    
    * Disables `WatchService` (pending watch refactor)
    
    * Completely disabled `WatchService` and other bits (pending watch refactor)
    
    * Adds WatchService implementation capable of watching arbitary resource types.
    
    - Splits some Operator ServiceManager logic out into Application -- pending Sidecar.
    - Fixes pluralisation issues with CRD definitions.
    - Moves EventBus into its own module, same with ServiceManager.
    - Changes Model Maven module to now clean before generating sources, fixing issues with jsonschema2pojo.
    - Reworks model JSON definitions slightly.
    
    * Updates Controller and HealthCheck services to work with new watch and cache changes.
    
    - Adds additional logging.
    
    * Fixes ConfigMap watch service now correctly watches ConfigMaps. Fixes VersionValidator to respect command line options.
    
    * Potentially fixes leaked OkHttp Response objects by explicitly closing the Watch.

--- operator/src/main/java/com/instaclustr/k8s/watch/WatchService.java
+++ operator/src/main/java/com/instaclustr/k8s/watch/WatchService.java
@@ -22,6 +22,7 @@ import io.kubernetes.client.util.Watch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -93,13 +94,12 @@ public class WatchService<ResourceT, ResourceListT, ResourceKeyT extends Key<Res
         }
     }
 
-    private void watchResourceList(final String listResourceVersion) throws ApiException {
+    private void watchResourceList(final String listResourceVersion) throws ApiException, IOException {
         logger.debug("Watching resource list.");
 
-        try {
-            currentCall = listCallProvider.get(listResourceVersion, true);
+        currentCall = listCallProvider.get(listResourceVersion, true);
 
-            final Watch<JsonObject> watch = Watch.createWatch(apiClient, currentCall, new TypeToken<Watch.Response<JsonObject>>() {}.getType());
+        try (final Watch<JsonObject> watch = Watch.createWatch(apiClient, currentCall, new TypeToken<Watch.Response<JsonObject>>() {}.getType())) {
 
             for (final Watch.Response<JsonObject> objectResponse : watch) {
                 final ResponseType responseType = ResponseType.valueOf(objectResponse.type);
