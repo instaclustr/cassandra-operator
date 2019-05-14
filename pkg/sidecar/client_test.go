d2397a3e6aa8bafdc15195caf815018cdb3406f8
Štefan Miklošovič <smiklosovic@users.noreply.github.com>
2019-05-14 22:14:16 +1000

    initial Sidecar client impl (#162)
    * initial Sidecar client impl
    
    * applied comments from review

---
+++ pkg/sidecar/client_test.go
@@ -0,0 +1,57 @@
+package sidecar
+
+import (
+	"os"
+	"testing"
+)
+
+func getHost() string {
+
+	host := os.Getenv("TEST_SIDECAR_HOST")
+
+	if len(host) == 0 {
+		return "192.168.56.104:4567"
+	} else {
+		return host
+	}
+}
+
+func TestSidecarClient_GetStatus(t *testing.T) {
+
+	client := NewSidecarClient(getHost(), &ClientOptions{Secure: false})
+
+	response, e := client.GetStatus()
+
+	if e != nil {
+		t.Errorf(e.Error())
+	} else if response == nil {
+		t.Errorf("GetStatus has not returned error but its response is not set.")
+	} else if response.OperationMode != NORMAL {
+		t.Errorf("GetStatus was successful but returned %s instead of %s", response.OperationMode, NORMAL)
+	}
+}
+
+func TestClient_DecommissionNode(t *testing.T) {
+
+	client := NewSidecarClient(getHost(), &ClientOptions{Secure: false})
+
+	// first decommissioning
+
+	response, err := client.DecommissionNode()
+
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	if response == nil {
+		t.Errorf("Decommissioning was successful but no DecommissionResponse was returned.")
+	}
+
+	// second decommissioning on the same node
+
+	response2, err2 := client.DecommissionNode()
+
+	if err2 == nil || response2 != nil {
+		t.Errorf("Decommissioning of already decomissioned node should fail.")
+	}
+}
