96ed5cdb868b965444afd9f983677481d4034bf5
Alex Lourie <djay.il@gmail.com>
2019-09-24 10:41:15 +0930

    Added more checks in CI
    Additional "repo" checks in CI will validate that OpenAPI and k8s
    objects are in sync with the code changes; as well as that `make bundle`
    was rerun and is also in sync with the current code state.
    
    Also, switch to a separate validation script vs having the code in
    circle CI config.
    
    Signed-off-by: Alex Lourie <djay.il@gmail.com>

---
+++ .circleci/validate_repo.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+set -uxo pipefail
+
+top_dir="$(git rev-parse --show-toplevel)"
+cd $top_dir
+
+echo "Validate repository state"
+
+function check-bundle {
+    make bundle
+    git diff --quiet
+    if [[ $? != 0 ]]; then
+        echo "Bundles were not properly built, make sure to run 'make bundle'"
+        exit 1
+    fi
+}
+
+function update-go-modules {
+    go get golang.org/x/tools/cmd/goimports && go mod download
+}
+
+function check-go-formatting {
+    files=$(goimports -l .)
+    if [ -n "$files" ]; then
+        echo "The following file(s) are not properly formatted or their imports are misaligned. Run go fmt/go imports on your code:"
+        echo "$files"
+        exit 1
+    fi
+}
+
+function install-operator-sdk {
+    RELEASE_VERSION=v0.10.0
+    curl -LO https://github.com/operator-framework/operator-sdk/releases/download/${RELEASE_VERSION}/operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu
+    curl -LO https://github.com/operator-framework/operator-sdk/releases/download/${RELEASE_VERSION}/operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu.asc
+    chmod +x operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu && mv operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu operator-sdk
+}
+
+function check-open-api {
+    ./operator-sdk generate openapi
+    git diff --quiet --exit-code deploy/crds/cassandraoperator_*_cassandra*_crd.yaml
+    if [[ $? != 0 ]]; then
+        echo "API files were not regenerated with latest changes, make sure to run 'operator-sdk generate openapi'"
+        exit 1
+    fi
+}
+
+function check-k8s {
+    ./operator-sdk generate k8s
+    git diff --quiet --exit-code pkg/apis
+    if [[ $? != 0 ]]; then
+        echo "k8s files were not regenerated with latest changes, make sure to run 'operator-sdk generate k8s'"
+        exit 1
+    fi
+}
+
+echo "Check bundles"
+check-bundle
+
+echo "Update go modules"
+update-go-modules
+
+echo "Check go formatting"
+check-go-formatting
+
+echo "Install operator-sdk"
+install-operator-sdk
+
+echo "Check OpenAPI"
+check-open-api
+
+echo "Check k8s"
+check-k8s
+
+echo "All clean, you may continue"
