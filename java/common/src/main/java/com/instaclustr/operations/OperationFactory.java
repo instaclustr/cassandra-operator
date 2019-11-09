a100f0912c113cd68c19ba2b4efd06bff79c1dbe
Stefan Miklosovic <stefan.miklosovic@instaclustr.com>
2019-11-09 16:53:16 +0100

    extracted java libs

--- java/common/src/main/java/com/instaclustr/operations/OperationFactory.java
+++
@@ -1,6 +0,0 @@
-package com.instaclustr.operations;
-
-@FunctionalInterface
-public interface OperationFactory<RequestT extends OperationRequest> {
-    Operation createOperation(final RequestT request);
-}
