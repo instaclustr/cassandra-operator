463ec8febd9d4eccd99c5705e1dfaa81cf8a4b4f
Stefan Miklosovic <stefan.miklosovic@instaclustr.com>
2020-04-16 21:18:40 +0200

    deleted Cassandra Sidecar as it is in separate repository

--- java/sidecar/src/main/java/com/instaclustr/cassandra/sidecar/operations/restart/RestartOperationRequest.java
+++
@@ -1,6 +0,0 @@
-package com.instaclustr.cassandra.sidecar.operations.restart;
-
-import com.instaclustr.operations.OperationRequest;
-
-public class RestartOperationRequest extends OperationRequest {
-}
